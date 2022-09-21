fd6e5c8a4ddaaa6168b3405c1b91eb79c8736ae0
navrez2113 <80328072+navrez2113@users.noreply.github.com>
2022-09-21 16:57:29 +0300

    first commit

---
+++ src/main/kotlin/com/example/gkproject/GkprojectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gkproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GkprojectApplication
+
+fun main(args: Array<String>) {
+	runApplication<GkprojectApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/gkproject/GkprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.gkproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GkprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
